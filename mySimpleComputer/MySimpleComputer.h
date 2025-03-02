367f840aa426fb80cf36a1a3437cdc1096377b9a
YaroslavGmyrya <jjaked25@gmail.com>
2025-03-02 21:10:55 +0300

    update format

--- mySimpleComputer/MySimpleComputer.h
+++ mySimpleComputer/MySimpleComputer.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h> 
 #include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
-#include <unistd.h>  
-#define MAX_LINES 10  
-#define MAX_STR_LEN 20 
-#define P 0x1 
-#define Z 0x2 
-#define M 0x4 
-#define T 0x8 
-#define E 0x10 
+#include <unistd.h> 
+#define MAX_LINES 10
+#define MAX_STR_LEN 20
+#define P 0x1
+#define Z 0x2
+#define M 0x4
+#define T 0x8
+#define E 0x10
 #define SIZE 128
-extern const int SIZE_COMMAND; 
+extern const int SIZE_COMMAND;
 extern int memory[SIZE];
 extern int accumulator;
 extern int command_counter;
